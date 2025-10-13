1a700cda89aecf0a37e046f178c5ca5001615468
Wendy-687 <s2776370@ed.ac.uk>
2025-10-13 13:02:43 +0000

    changed

--- week03/week4.py
+++ week03/week4.py
@@ -1,4 +1,4 @@
-def first_fit(item_list, bin_size):
+def first_fit(item_list, bin_size,method='decreasing'):
     '''
     First-fit algorithm for the bin packing problem.
     Input:
@@ -12,10 +12,17 @@ def first_fit(item_list, bin_size):
     bins = [0]
 
     # Sort items by decreasing size
-    sorted_items = sorted(item_list,reverse=True)
-
+    if method == "decreasing":
+        sorted_items = sorted(item_list,reverse=True)
+    elif method == 'increasing':
+        sorted_items = sorted(item_list)
+    elif method == 'none':
+        sorted_items = item_list.copy()
+    else:
+        raise ValueError(f'method is {method},should be "increasing","decreasing","none"')
     # Loop over the items
     for item in item_list:
+        placed = False
         # Loop over the open bins
         for b in range(len(bins)):
 
@@ -23,11 +30,14 @@ def first_fit(item_list, bin_size):
             if item + bins[b] <= bin_size:
                 #Yes, place the item
                 bins[b] += item
+                placed = True
+                break
 
         # If item has not been placed,
         # Start a new bin and place it there
-        if ...:
-            bins.append(item)
+        if not placed:
+        # if placed == False:
+        #if (not placed) == True:
             
     return bins
 
@@ -35,7 +45,7 @@ def first_fit(item_list, bin_size):
 
 
 ## Testing
-
+import numpy as np
 # List of item sizes
 item_list = [2, 1, 3, 2, 1, 2, 3, 1]
 
@@ -50,3 +60,17 @@ bins_expected = [4, 4, 4, 3]
 # but raises an error if X is False, optionally with an error message (string)
 msg = f'Incorrect result: expected {bins_expected}, got {bins_result} instead.'
 assert bins_result == bins_expected, msg
+print('Test passed.')
+
+## Which method is better: sorting up, down, or not?
+# Count the number of bins used for the 3 different methods.
+
+# Generated lots of items
+number_of_sets = 100
+number_of_items = 30
+bin_sizes = 5 * np.random.rand(number_of_sets) + 5
+
+item_sets = 10 * np.random.rand(number_of_sets, number_of_items)
+
+for i in range(number_of_sets):
+    bins_decreasing = first_fit(item_sets[i, :])
